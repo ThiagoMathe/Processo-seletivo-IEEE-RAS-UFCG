418e1b0fe6821c33cd331aa2fba9c456d705bad1
Thiago Matheus Honorato <thiago.honorato@academico.ifpb.edu.br>
2023-04-05 19:29:39 -0300

    Create pixels-substituídos-pela-cor-azul.py

---
+++ Missao-OpenCV/pixels-substituídos-pela-cor-azul.py
@@ -0,0 +1,7 @@
+import cv2
+imagem = cv2.imread('Ponte.jpg')
+for y in range(0, imagem.shape[0]):
+ for x in range(0, imagem.shape[1]):
+     imagem[y, x] = (255,0,0)
+cv2.imshow("Imagem modificada", imagem)
+cv2.waitKey(0)
